ca54251530065be797d293159e5d52b6b03b75bd
Mauricio-25 <mau252004@gmail.com>
2024-06-14 15:29:04 -0500

    Añadido una articulacion mas (cuerpo)

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -13,9 +13,10 @@ function Index() {
       <div className='h-full relative grid grid-rows-[88%_12%] md:w-[650px] md:bg-white overflow-hidden'>
         <main className='h-full overflow-y-auto scroll'>
           <Header />
-          <Section h2="Base" component={<Input />} />
-          <Section h2="Codo" component={<Input isVetical />} />
           <Section h2="Garra" component={<Button isVetical />} />
+          <Section h2="Brazo" component={<Input isVetical />} />
+          <Section h2="Cuerpo" component={<Input isVetical />} />
+          <Section h2="Base" component={<Input />} />
         </main>
         <Nav firstSecActive></Nav>
       </div>
